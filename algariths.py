84427209e8dbe11f207bc3b3b669e7df5b0bea3f
Lewsi_repo211 <danielsmarquise87@gmail.com>
2023-03-29 14:44:20 -0400

    main.py

---
+++ algariths.py
@@ -0,0 +1,49 @@
+word = 'Tommy'
+reversed_word = word[4] +  word[3] + word[2] + word[1] + word[0]
+print(reversed_word)
+
+
+
+def reverse_word (word):
+    return word[::-1]
+
+
+
+
+
+str = "hello world"
+print(str.capitalize())
+
+
+
+
+
+
+
+
+s = input("Hello World : ")
+
+reverses  = s[::-1]
+
+if(s == reverse):
+    print("yes it is palindrome")
+else:
+    print("no this is not palindrom")
+ 
+
+
+        
+        
+    def is_palindrome(word):
+        reversed_word = reverse_word(word())
+        if word() == reversed_word:
+            return True
+        else:
+            return False
+        
+        print(is_palindrome('tommy'))
+        print(is_palindrome('racecar'))
+        print(is_palindrome('bool'))
+
+
+
